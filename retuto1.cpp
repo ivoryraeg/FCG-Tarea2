189078ff24633a2312fd13ebaf3f6db1b4960557
Kn4ll-Nk <68975635+Kn4ll-Nk@users.noreply.github.com>
2021-06-22 21:37:02 -0400

    Encapsulamiento Código Muros

--- retuto1.cpp
+++ retuto1.cpp
@@ -111,6 +111,23 @@ class Muro {
     public:
         Triangle up;
         Triangle down;
+        GLuint vertexBuffersID[2];
+        GLuint normalBuffersID[2];
+        GLuint UVBuffersID[2];
+        GLfloat vertexBufferUp[9];
+        GLfloat vertexBufferDown[9];
+        GLfloat normalBufferUp[9];
+        GLfloat normalBufferDown[9];
+        GLfloat UVBufferUp[6] = { 
+            0.0f, 1.0f-0.0f,
+            0.0f, 1.0f-1.0f,
+            1.0f, 1.0f-1.0f
+        };
+        GLfloat UVBufferDown[6] = { 
+            0.0f, 1.0f-0.0f,
+            1.0f, 1.0f-1.0f,
+            0.0f, 1.0f-1.0f
+        };
         Muro(vec3 upLeft, vec3 upRight, vec3 downLeft, vec3 downRight) {
             up.vertices[0] = downLeft;
             up.vertices[1] = upLeft;
@@ -119,6 +136,81 @@ class Muro {
             down.vertices[0] = downLeft;
             down.vertices[1] = upRight;
             down.vertices[3] = downRight;
+
+            glGenBuffers(2, UVBuffersID);
+            glBindBuffer(GL_ARRAY_BUFFER, UVBuffersID[0]);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(UVBufferUp), UVBufferUp, GL_STATIC_DRAW);
+            glBindBuffer(GL_ARRAY_BUFFER, UVBuffersID[1]);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(UVBufferDown), UVBufferDown, GL_STATIC_DRAW);
+
+            glGenBuffers(2, normalBuffersID);
+            glBindBuffer(GL_ARRAY_BUFFER, normalBuffersID[0]);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(normalBufferUp), normalBufferUp, GL_STATIC_DRAW);
+            glBindBuffer(GL_ARRAY_BUFFER, normalBuffersID[1]);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(normalBufferDown), normalBufferDown, GL_STATIC_DRAW);
+
+            glGenBuffers(2, vertexBuffersID);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffersID[0]);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(vertexBufferUp), vertexBufferUp, GL_STATIC_DRAW);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffersID[1]);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(vertexBufferDown), vertexBufferDown, GL_STATIC_DRAW);
+        }
+
+        void Draw() {
+            //glUseProgram(programUI);
+            // Draw triangle...
+            up.PassToBuffer(vertexBufferUp, normalBufferUp);
+            down.PassToBuffer(vertexBufferDown, normalBufferDown);
+            for(int i=0;i<2;i++){
+                auto g_vertex = vertexBufferUp;
+                auto g_uv = UVBufferUp;
+                if(i==1){
+                    g_vertex = vertexBufferDown;
+                    g_uv = UVBufferDown; 
+                }
+
+                // 1st attribute buffer : vertices
+                glEnableVertexAttribArray(0);
+                glBindBuffer(GL_ARRAY_BUFFER, vertexBuffersID[i]);
+                //glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex), g_vertex, GL_STATIC_DRAW);
+                glVertexAttribPointer(
+                    0,        // attribute 0. No particular reason for 0, but must match the layout in the shader.
+                    3,        // size
+                    GL_FLOAT, // type
+                    GL_FALSE, // normalized?
+                    0,        // stride
+                    (void *)0 // array buffer offset
+                );    
+                
+                // 2st attribute buffer : uv
+                glEnableVertexAttribArray(1);        
+                glBindBuffer(GL_ARRAY_BUFFER, UVBuffersID[i]);
+                //glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv), g_uv, GL_STATIC_DRAW);
+                glVertexAttribPointer(
+                    1,        // attribute 1. No particular reason for 0, but must match the layout in the shader.
+                    2,        // size
+                    GL_FLOAT, // type
+                    GL_FALSE, // normalized?
+                    0,        // stride
+                    (void *)0 // array buffer offset
+                );    
+                glEnableVertexAttribArray(2);
+                glBindBuffer(GL_ARRAY_BUFFER, normalBuffersID[i]);
+                //Normals
+                glVertexAttribPointer(
+                    2,        // attribute 0. No particular reason for 0, but must match the layout in the shader.
+                    3,        // size
+                    GL_FLOAT, // type
+                    GL_FALSE, // normalized?
+                    0,        // stride
+                    (void *)0 // array buffer offset
+                );   
+                // Draw the triangle !
+                glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+                glDisableVertexAttribArray(0);
+                glDisableVertexAttribArray(1);
+                glDisableVertexAttribArray(2);
+            }    
         }
 };
 
@@ -158,7 +250,7 @@ static const GLfloat g_uv_buffer_data[] = {
     1.0f, 1.0f-0.0f,
     1.0f, 1.0f-1.0f
 };
-static const GLfloat g_uv_buffer_dataUI1[] = { /***************************/
+static const GLfloat g_uv_buffer_dataUI1[] = { 
     0.0f, 1.0f-0.0f,
     0.0f, 1.0f-1.0f,
     1.0f, 1.0f-1.0f
@@ -522,61 +614,12 @@ void Scene2(double deltaTime, GLFWwindow *window)
     
 }
 
-void Scene3(double deltaTime,GLFWwindow *window)
-{
+void Scene3(double deltaTime,GLFWwindow *window) {
     glUseProgram(programID3);
     static float yDirection = 1;
     static Muro *muro = new Muro(vec3(-1, 1, 0), vec3(1, 1, 0), vec3(-1, -1, 0), vec3(1, -1, 0));
-    muro->up.PassToBuffer(g_vertex_buffer_data1, g_normal_buffer_data1);
-    
-    // Draw triangle...
-
-    // 1st attribute buffer : vertices
-    glEnableVertexAttribArray(0);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_DRAW_BUFFER);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer[0]);     
-    glVertexAttribPointer(
-        0,        // attribute 0. No particular reason for 0, but must match the layout in the shader.
-        3,        // size
-        GL_FLOAT, // type
-        GL_FALSE, // normalized?
-        0,        // stride
-        (void *)0 // array buffer offset
-    );
- 
-    glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-    glVertexAttribPointer(
-        1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-        2,                                // size : U+V => 2
-        GL_FLOAT,                         // type
-        GL_FALSE,                         // normalized?
-        0,                                // stride
-        (void*)0                          // array buffer offset
-    );
-
-     //3rd attribute buffer : normals
-    glEnableVertexAttribArray(2);
-    glBindBuffer(GL_ARRAY_BUFFER, normalbuffer[0]);
-    glVertexAttribPointer(
-        2,                                // attribute
-        3,                                // size
-        GL_FLOAT,                         // type
-        GL_FALSE,                         // normalized?
-        0,                                // stride
-        (void*)0                          // array buffer offset
-    );
-
-    // Draw the triangle !
-      
-    glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
-    glDisableVertexAttribArray(0);
-    glDisableVertexAttribArray(1);
-    glDisableVertexAttribArray(2);
-
-    triangle1.PassToBuffer(g_vertex_buffer_data1);
-    triangle2.PassToBuffer(g_vertex_buffer_data2);
-
+    glUniform1i(glGetUniformLocation(programUI,"myTextureSampler"),2);
+    muro->Draw();
 }
 
 int main()
